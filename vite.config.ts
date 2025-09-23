80b772d38be581c577e9839a393afa31e47bf4b2
majid <rustammajid76@gmail.com>
2025-09-24 02:27:52 +0700

    tes

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,34 @@
-import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import monacoEditorEsmPlugin from 'vite-plugin-monaco-editor-esm'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import monacoEditorEsmPlugin from "vite-plugin-monaco-editor-esm";
 export default defineConfig(({ mode }) => ({
-	plugins: [
-		mode === 'production' &&
-			sentryVitePlugin({
-				org: 'cozyblog',
-				project: 'easyrd-web',
-				authToken: process.env.SENTRY_AUTH_TOKEN
-			}),
-		sveltekit(),
-		monacoEditorEsmPlugin({
-			languageWorkers: ['editorWorkerService','typescript', 'css', 'html', 'json']
-		})
-	].filter(Boolean),
-	resolve: { alias: { $lib: '/src/lib' } },
-	ssr: { 
-		noExternal: ['@sentry/sveltekit']
-	},
-	optimizeDeps: {
-		include: ['@sentry/sveltekit','monaco-editor']
-	},
-	assetsInclude: ['**/*.css']
+  plugins: [
+    mode === "production" &&
+      sentryVitePlugin({
+        org: "cozyblog",
+        project: "easyrd-web",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      }),
+    sveltekit(),
+    monacoEditorEsmPlugin({
+      languageWorkers: [
+        "editorWorkerService",
+        "typescript",
+        "css",
+        "html",
+        "json",
+      ],
+    }),
+  ].filter(Boolean),
+  resolve: { alias: { $lib: "/src/lib" } },
+  ssr: {
+    noExternal: ["@sentry/sveltekit"],
+  },
+  optimizeDeps: {
+    include: ["@sentry/sveltekit", "monaco-editor"],
+  },
+  css: {
+    postcss: "./postcss.config.cjs",
+  },
 }));
